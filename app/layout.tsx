3e72239ca7bcf928b7a6cfadc4c38b028f49a886
anitaparmar26 <easeanita@gmail.com>
2025-09-29 16:08:24 +0530

    added title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Ai agent landing page',
+  description: 'Automate complex workflows, make data-driven decisions, and solve challenging problems effortlessly with our agentic AI platform. Empower your business with intelligent automation and streamlined processes designed for modern enterprises.',
   generator: 'v0.app',
 }
 
